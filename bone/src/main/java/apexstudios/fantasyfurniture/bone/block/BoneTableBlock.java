4534c295852f8341e34604146b2e25dbbef48f7b
Apex <robsonlawson3@gmail.com>
2025-03-06 07:35:11 +0000

    Fix bone table hitbox having initial rotation

--- bone/src/main/java/apexstudios/fantasyfurniture/bone/block/BoneTableBlock.java
+++ bone/src/main/java/apexstudios/fantasyfurniture/bone/block/BoneTableBlock.java
@@ -11,9 +11,9 @@ public final class BoneTableBlock extends TableBlock {
     public static final VoxelShape SHAPE_TABLE_TOP = box(0D, 14D, 0D, 16D, 16D, 16D);
 
     public static final VoxelShape SHAPE_TABLE_LEG = ApexShapes.join(
-            box(1D, 0D, 1D, 4D, 2D, 4D),
-            box(1D, 12D, 1D, 4D, 14D, 4D),
-            box(1.5D, 2D, 1.5D, 3.5D, 12D, 3.5D)
+            box(12D, 12D, 1D, 15D, 14D, 4D),
+            box(12D, 0D, 1D, 15D, 2D, 4D),
+            box(12.5D, 2D, 1.5D, 14.5D, 13D, 3.5D)
     );
 
     public BoneTableBlock(Properties properties) {
