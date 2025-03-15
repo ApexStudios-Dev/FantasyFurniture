26c069e4b702528ab9a86d6c2ea0ef3392d227c9
Apex <robsonlawson3@gmail.com>
2025-03-15 06:49:49 +0000

    Add berry baskets
    Add initial ported asset files

--- decorations/src/main/java/dev/apexstudios/fantasyfurniture/decorations/DecorationsModule.java
+++ decorations/src/main/java/dev/apexstudios/fantasyfurniture/decorations/DecorationsModule.java
@@ -1,8 +1,27 @@
 package dev.apexstudios.fantasyfurniture.decorations;
 
+import dev.apexstudios.apexcore.lib.registree.Registree;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.CreativeModeTab;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
 @Mod(DecorationsModule.ID)
 public final class DecorationsModule {
     public static final String ID = "fantasyfurniture_decorations";
+    public static final Registree REGISTREE = new Registree(ID);
+
+    public static final ResourceKey<CreativeModeTab> CREATIVE_MODE_TAB_KEY = REGISTREE.registerCreativeModeTab("items", () -> DecoItems.BERRY_BASKET_EMPTY.toStack(), (parameters, output) -> REGISTREE
+            .stream(Registries.ITEM)
+            .filter(item -> item.isEnabled(parameters.enabledFeatures()))
+            .forEach(output::accept)
+    );
+
+    public DecorationsModule(IEventBus modBus) {
+        DecoBlocks.register();
+        DecoItems.register();
+
+        REGISTREE.registerEvents(modBus);
+    }
 }
---
+++ decorations/src/main/java/dev/apexstudios/fantasyfurniture/decorations/block/BerryBasketBlock.java
@@ -0,0 +1,19 @@
+package dev.apexstudios.fantasyfurniture.decorations.block;
+
+import dev.apexstudios.apexcore.lib.component.ComponentRegistrar;
+import dev.apexstudios.apexcore.lib.component.block.BlockComponent;
+import dev.apexstudios.apexcore.lib.component.block.types.FacingBlockComponent;
+import dev.apexstudios.fantasyfurniture.block.base.FurnitureBlockComponentHolder;
+
+public final class BerryBasketBlock extends FurnitureBlockComponentHolder {
+    public BerryBasketBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    protected void registerComponents(ComponentRegistrar<BlockComponent> registrar) {
+        super.registerComponents(registrar);
+
+        FacingBlockComponent.registerHorizontal(registrar);
+    }
+}
