e9ac62961dfe59e94b250b059d5adbb7f837b1bf
Apex <robsonlawson3@gmail.com>
2025-05-21 21:16:04 +0100

    Add missing oven hitboxes

--- venthyr/src/main/java/dev/apexstudios/fantasyfurniture/venthyr/block/VenthyrOvenBlock.java
+++ venthyr/src/main/java/dev/apexstudios/fantasyfurniture/venthyr/block/VenthyrOvenBlock.java
@@ -1,10 +1,22 @@
 package dev.apexstudios.fantasyfurniture.venthyr.block;
 
+import dev.apexstudios.apexcore.lib.util.ApexShapes;
 import dev.apexstudios.fantasyfurniture.block.OvenBlock;
-import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public final class VenthyrOvenBlock extends OvenBlock {
+    public static final VoxelShape SHAPE = ApexShapes.join(
+            box(1D, 0D, 1D, 4D, 2D, 4D),
+            box(1D, 0D, 12D, 4D, 2D, 15D),
+            box(12D, 0D, 12D, 15D, 2D, 15D),
+            box(12D, 0D, 1D, 15D, 2D, 4D),
+            box(0D, 2D, 0D, 16D, 4D, 16D),
+            box(0D, 14D, 0D, 16D, 16D, 16D),
+            box(1D, 4D, 1D, 15D, 14D, 15D),
+            box(3D, 5D, 0D, 13D, 13D, 1D)
+    );
+
     public VenthyrOvenBlock(Properties properties) {
-        super(properties, Shapes.block()); // TODO
+        super(properties, SHAPE);
     }
 }
