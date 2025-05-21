dd22c318663a03d43a31ce5854e1fca1ee0b63d1
Apex <robsonlawson3@gmail.com>
2025-05-21 21:21:32 +0100

    Add missing venthyr hitboxes

--- venthyr/src/main/java/dev/apexstudios/fantasyfurniture/venthyr/block/VenthyrChandelierBlock.java
+++ venthyr/src/main/java/dev/apexstudios/fantasyfurniture/venthyr/block/VenthyrChandelierBlock.java
@@ -1,10 +1,12 @@
 package dev.apexstudios.fantasyfurniture.venthyr.block;
 
 import dev.apexstudios.fantasyfurniture.block.ChandelierBlock;
-import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public final class VenthyrChandelierBlock extends ChandelierBlock {
+    public static final VoxelShape SHAPE = box(1D, 0D, 1D, 15, 16D, 15D);
+
     public VenthyrChandelierBlock(Properties properties) {
-        super(properties, Shapes.block()); // TODO
+        super(properties, SHAPE);
     }
 }
