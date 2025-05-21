ce495f0a20564d0ed93aedfe4695eb3e64b36999
Apex <robsonlawson3@gmail.com>
2025-05-21 21:12:05 +0100

    Add missing counter hitboxes

--- bone/src/main/java/apexstudios/fantasyfurniture/bone/block/BoneOvenBlock.java
+++ bone/src/main/java/apexstudios/fantasyfurniture/bone/block/BoneOvenBlock.java
@@ -1,10 +1,24 @@
 package apexstudios.fantasyfurniture.bone.block;
 
+import dev.apexstudios.apexcore.lib.util.ApexShapes;
 import dev.apexstudios.fantasyfurniture.block.OvenBlock;
-import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public final class BoneOvenBlock extends OvenBlock {
+    public static final VoxelShape SHAPE = ApexShapes.join(
+            box(0D, 0D, 0D, 3D, 2D, 3D),
+            box(0D, 0D, 13D, 3D, 2D, 16D),
+            box(13D, 0D, 13D, 16D, 2D, 16D),
+            box(13D, 0D, 0D, 16D, 2D, 3D),
+            box(13D, 12D, 0D, 16D, 14D, 3D),
+            box(13D, 12D, 13D, 16D, 14D, 16D),
+            box(0D, 12D, 13D, 3D, 14D, 16D),
+            box(0D, 12D, 0D, 3D, 14D, 3D),
+            box(.5D, .5D, .5D, 15.5D, 14D, 15.5D),
+            box(0D, 14D, 0D, 16D, 16D, 16D)
+    );
+
     public BoneOvenBlock(Properties properties) {
-        super(properties, Shapes.block()); // TODO
+        super(properties, SHAPE);
     }
 }
--- venthyr/src/main/java/dev/apexstudios/fantasyfurniture/venthyr/block/VenthyrCounterBlock.java
+++ venthyr/src/main/java/dev/apexstudios/fantasyfurniture/venthyr/block/VenthyrCounterBlock.java
@@ -1,10 +1,23 @@
 package dev.apexstudios.fantasyfurniture.venthyr.block;
 
+import dev.apexstudios.apexcore.lib.util.ApexShapes;
 import dev.apexstudios.fantasyfurniture.block.CounterBlock;
-import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public final class VenthyrCounterBlock extends CounterBlock {
+    public static final VoxelShape CORNER_SHAPE = ApexShapes.join(
+            box(0D, 0D, 3D, 16D, 13D, 16D),
+            box(0D, 13D, 0D, 16D, 16D, 16D),
+            box(1D, 1D, 2D, 15D, 12D, 3D)
+    );
+
+    public static final VoxelShape SHAPE = ApexShapes.join(
+            box(0D, 0D, 3D, 16D, 13D, 16D),
+            box(0D, 13D, 0D, 16D, 16D, 16D),
+            box(1D, 1D, 2D, 15D, 12D, 3D)
+    );
+
     public VenthyrCounterBlock(Properties properties) {
-        super(properties, Shapes.block(), Shapes.block()); // TODO
+        super(properties, SHAPE, CORNER_SHAPE);
     }
 }
