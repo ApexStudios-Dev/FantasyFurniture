af7acac008f6ace01c60af19d3e252236ae3a440
Apex <robsonlawson3@gmail.com>
2025-05-21 20:42:37 +0100

    Fix wood types not being registered sometimes

--- src/main/java/dev/apexstudios/fantasyfurniture/util/FurnitureUtil.java
+++ src/main/java/dev/apexstudios/fantasyfurniture/util/FurnitureUtil.java
@@ -406,6 +406,11 @@ public interface FurnitureUtil {
             registerPoi(registree, PoiTypes.HOME, Names.BED_SINGLE, blockState -> blockState.getValue(BedBlock.PART) == BedPart.HEAD);
             registerPoi(registree, PoiTypes.HOME, Names.BED_DOUBLE, blockState -> blockState.getValue(BedBlock.PART) == BedPart.HEAD);
             registerPoi(registree, PoiTypes.BUTCHER, Names.OVEN, Predicates.alwaysTrue());
+
+            // on random launches our wood types are not being registered correctly
+            // leading to null wood types during game initialization
+            // doubly register to attempt to fix this
+            WoodType.register(woodType);
         }));
 
         modBus.addListener(RegisterClientExtensionsEvent.class, event -> event.registerBlock(
