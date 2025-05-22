dcfcf4d7a319524ecc125f0c3ea954c4c48b599b
Apex <robsonlawson3@gmail.com>
2025-05-22 04:13:58 +0100

    Add more sign and trapdoor textures

---
+++ bone/src/main/java/apexstudios/fantasyfurniture/bone/WitherFurnitureSetClientEntryPoint.java
@@ -0,0 +1,12 @@
+package apexstudios.fantasyfurniture.bone;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(value = WitherFurnitureSet.ID, dist = Dist.CLIENT)
+public final class WitherFurnitureSetClientEntryPoint {
+    public WitherFurnitureSetClientEntryPoint(IEventBus modBus) {
+        BoneFurnitureSetClient.register(WitherFurnitureSet.get(), modBus);
+    }
+}
