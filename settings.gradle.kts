57bb57756dc4649b1960ceed89aca562dc29e003
Apex <robsonlawson3@gmail.com>
2025-06-19 13:52:34 +0100

    Bump ApexCore

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 
 dependencyResolutionManagement {
     versionCatalogs.create("libs") {
-        library("apexcore", "dev.apexstudios", "apexcore").version("21.6.6-beta-pr-43")
+        library("apexcore", "dev.apexstudios", "apexcore").version("21.6.7-beta-pr-43")
         library("contex", "xfacthd.contex", "ConnectedTextures").version("9.1")
     }
 }
