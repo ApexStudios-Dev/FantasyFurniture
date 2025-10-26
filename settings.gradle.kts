d0f4903312e472e684c0ff57e41f40a3e156667b
Apex <robsonlawson3@gmail.com>
2025-10-26 13:49:08 +0000

    Bump ApexCore version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,9 +8,9 @@ pluginManagement {
 
 dependencyResolutionManagement {
     versionCatalogs.create("libs") {
-        library("registree", "dev.apexstudios", "registree").version("21.10.2-beta-pr-1") // match version ApexCore is compiled against
-        library("placementvisualizer", "dev.apexstudios", "placementvisualizer").version("21.10.3-beta-pr-1") // match version ApexCore is compiled against
-        library("apexcore", "dev.apexstudios", "apexcore").version("21.10.3-beta-pr-54")
+        library("registree", "dev.apexstudios", "registree").version("21.10.2") // match version ApexCore is compiled against
+        library("placementvisualizer", "dev.apexstudios", "placementvisualizer").version("21.10.1") // match version ApexCore is compiled against
+        library("apexcore", "dev.apexstudios", "apexcore").version("21.10.5-beta-pr-54")
         library("contex", "curse.maven", "contex-1296805").version("7051640")
     }
 }
