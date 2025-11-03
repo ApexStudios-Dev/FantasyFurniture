b8ea813dc18d2870977b768c882c4077516f6f6e
Apex <robsonlawson3@gmail.com>
2025-11-03 00:07:21 +0000

    initial decorations module setup

---
+++ decorations/src/data/java/dev/apexstudios/fantasyfurniture/decorations/data/DecorationsFurnitureModuleDataEntryPoint.java
@@ -0,0 +1,15 @@
+package dev.apexstudios.fantasyfurniture.decorations.data;
+
+import dev.apexstudios.apexcore.lib.data.ResourceGenerator;
+import dev.apexstudios.fantasyfurniture.decorations.DecorationsFurnitureModule;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(DecorationsFurnitureModule.ID)
+public final class DecorationsFurnitureModuleDataEntryPoint {
+    public DecorationsFurnitureModuleDataEntryPoint(IEventBus modBus) {
+        ResourceGenerator.of(modBus, generator -> {
+            var pack = generator.pack();
+        });
+    }
+}
