e7f56ee612f6365429605924855e265aa03f1515
Apex <robsonlawson3@gmail.com>
2025-11-19 12:02:05 +0000

    Cleanup berry basket registration

--- decorations/src/main/java/dev/apexstudios/fantasyfurniture/decorations/util/DecorationUtil.java
+++ decorations/src/main/java/dev/apexstudios/fantasyfurniture/decorations/util/DecorationUtil.java
@@ -1,17 +1,12 @@
 package dev.apexstudios.fantasyfurniture.decorations.util;
 
+import dev.apexstudios.fantasyfurniture.decorations.block.BerryBasketBlock;
 import dev.apexstudios.fantasyfurniture.util.FurnitureUtil;
 import dev.apexstudios.registree.api.Registree;
 import dev.apexstudios.registree.api.holder.DeferredBlock;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 
 public interface DecorationUtil {
-    Supplier<BlockBehaviour.Properties> BERRY_BASKET_PROPERTIES = FurnitureUtil.PLANK_PROPERTIES;
-
-    static <TBlock extends Block> DeferredBlock<TBlock> berryBasket(Registree registree, String berryType, Function<BlockBehaviour.Properties, TBlock> factory) {
-        return FurnitureUtil.simpleBlock(registree, berryType + "_basket", factory, BERRY_BASKET_PROPERTIES);
+    static DeferredBlock<BerryBasketBlock> berryBasket(Registree registree, String berryType) {
+        return FurnitureUtil.simpleBlock(registree, berryType + "_basket", BerryBasketBlock::new, FurnitureUtil.PLANK_PROPERTIES);
     }
 }
