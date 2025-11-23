558fa90ef111de38ec9c4bf8c9da09f3a3d4b44c
Apex <robsonlawson3@gmail.com>
2025-11-23 21:24:48 +0000

    Add potion bottles

--- decorations/src/main/java/dev/apexstudios/fantasyfurniture/decorations/block/PotionBottlesBlock.java
+++ decorations/src/main/java/dev/apexstudios/fantasyfurniture/decorations/block/PotionBottlesBlock.java
@@ -12,20 +12,27 @@ import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-public final class FloatingTomesBlock extends StackedDyeableBlock implements Dyeable.Colored {
-    public static final IntegerProperty COUNT = IntegerProperty.create("count", 0, 1);
+public final class PotionBottlesBlock extends StackedDyeableBlock implements Dyeable.WithNone {
+    public static final IntegerProperty COUNT = IntegerProperty.create("count", 0, 2);
 
-    public static final VoxelShape SHAPE_0 = box(2.5, 1.5, 4, 15.5, 5, 14);
+    public static final VoxelShape SHAPE_0 = box(6D, 0D, 6D, 10D, 10D, 10D);
 
     public static final VoxelShape SHAPE_1 = ApexShapes.join(
-            box(2.5D, 1.5D, 4D, 15.5D, 5D, 14D),
-            box(3D, 7.5D, -.5D, 12D, 11D, 12.5D)
+            box(10D, 0D, 7D, 14D, 10D, 11D),
+            box(1.25D, 0D, 3.25D, 6.75D, 10D, 8.75D)
+    );
+
+    public static final VoxelShape SHAPE_2 = ApexShapes.join(
+            box(10D, 0D, 10D, 14D, 10D, 14D),
+            box(1.25D, 0D, 6.25D, 6.75D, 10D, 11.75D),
+            box(6.25D, 0D, 1.25D, 11.75D, 10D, 6.75D)
     );
 
     public static final Map<Direction, VoxelShape> SHAPES_0 = Shapes.rotateHorizontal(SHAPE_0);
     public static final Map<Direction, VoxelShape> SHAPES_1 = Shapes.rotateHorizontal(SHAPE_1);
+    public static final Map<Direction, VoxelShape> SHAPES_2 = Shapes.rotateHorizontal(SHAPE_2);
 
-    public FloatingTomesBlock(Properties properties) {
+    public PotionBottlesBlock(Properties properties) {
         super(properties);
     }
 
@@ -37,6 +44,7 @@ public final class FloatingTomesBlock extends StackedDyeableBlock implements Dye
         return (switch(count) {
             case 0 -> SHAPES_0;
             case 1 -> SHAPES_1;
+            case 2 -> SHAPES_2;
             default -> throw new IllegalStateException("Unexpected value: " + count);
         }).get(facing);
     }
