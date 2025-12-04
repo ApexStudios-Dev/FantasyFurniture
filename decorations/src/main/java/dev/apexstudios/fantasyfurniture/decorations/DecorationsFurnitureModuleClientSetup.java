2c8c3084c26a2ba9e21e6e7cf6bc03892a00662f
Apex <robsonlawson3@gmail.com>
2025-12-04 18:31:12 +0000

    Add widow bloom and skull blossom blocks

--- decorations/src/main/java/dev/apexstudios/fantasyfurniture/decorations/DecorationsFurnitureModuleClientSetup.java
+++ decorations/src/main/java/dev/apexstudios/fantasyfurniture/decorations/DecorationsFurnitureModuleClientSetup.java
@@ -1,6 +1,9 @@
 package dev.apexstudios.fantasyfurniture.decorations;
 
 import dev.apexstudios.apexcore.lib.block.Dyeable;
+import dev.apexstudios.fantasyfurniture.decorations.ber.DecorLayerDefinitions;
+import dev.apexstudios.fantasyfurniture.decorations.ber.SimpleBlockEntityRenderer;
+import dev.apexstudios.fantasyfurniture.decorations.ber.SimpleBlockEntitySpecialRenderer;
 import dev.apexstudios.fantasyfurniture.decorations.grave.GravestoneBlockEntity;
 import dev.apexstudios.fantasyfurniture.decorations.grave.GravestoneBlockEntityRenderer;
 import dev.apexstudios.fantasyfurniture.decorations.grave.GravestoneEditScreen;
@@ -16,6 +19,7 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.client.event.RegisterColorHandlersEvent;
+import net.neoforged.neoforge.client.event.RegisterSpecialModelRendererEvent;
 import net.neoforged.neoforge.client.event.ScreenEvent;
 import net.neoforged.neoforge.common.NeoForge;
 
@@ -29,7 +33,26 @@ public final class DecorationsFurnitureModuleClientSetup {
         ));
 
         modBus.addListener(RegisterColorHandlersEvent.Block.class, event -> Dyeable.registerBlockColor(DecorationsFurnitureModule.REGISTREE, event));
-        modBus.addListener(EntityRenderersEvent.RegisterRenderers.class, event -> event.registerBlockEntityRenderer(DecorationsFurnitureModule.GRAVESTONE_BLOCK_ENTITY.value(), GravestoneBlockEntityRenderer::new));
+
+        modBus.addListener(EntityRenderersEvent.RegisterRenderers.class, event -> {
+            event.registerBlockEntityRenderer(DecorationsFurnitureModule.GRAVESTONE_BLOCK_ENTITY.value(), GravestoneBlockEntityRenderer::new);
+            event.registerBlockEntityRenderer(DecorationsFurnitureModule.WIDOW_BLOOM_BLOCK_ENTITY.value(), SimpleBlockEntityRenderer.create(DecorLayerDefinitions.WIDOW_BLOOM, blockEntity -> DecorLayerDefinitions.WIDOW_BLOOM_TEXTURE));
+
+            event.registerBlockEntityRenderer(DecorationsFurnitureModule.SKULL_BLOSSOM_BLOCK_ENTITY.value(), SimpleBlockEntityRenderer.create(
+                    DecorLayerDefinitions.SKULL_BLOSSOM,
+                    blockEntity -> DecorationsFurnitureModule.SKULL_BLOSSOM_SKELETON_BLOCK.is(blockEntity.getBlockState()) ? DecorLayerDefinitions.SKULL_BLOSSOM_SKELETON_TEXTURE : DecorLayerDefinitions.SKULL_BLOSSOM_WITHER_TEXTURE
+            ));
+        });
+
+        modBus.addListener(RegisterSpecialModelRendererEvent.class, event -> {
+            event.register(DecorationsFurnitureModule.identifier("widow_bloom"), SimpleBlockEntitySpecialRenderer.WidowBloom.MAP_CODEC);
+            event.register(DecorationsFurnitureModule.identifier("skull_blossom"), SimpleBlockEntitySpecialRenderer.SkullBlossom.MAP_CODEC);
+        });
+
+        modBus.addListener(EntityRenderersEvent.RegisterLayerDefinitions.class, event -> {
+            event.registerLayerDefinition(DecorLayerDefinitions.WIDOW_BLOOM, DecorLayerDefinitions::createWidowBloom);
+            event.registerLayerDefinition(DecorLayerDefinitions.SKULL_BLOSSOM, DecorLayerDefinitions::createSkullBlossom);
+        });
 
         NeoForge.EVENT_BUS.addListener(ScreenEvent.Opening.class, event -> {
             if(event.getNewScreen() instanceof AbstractSignEditScreen screen && !(screen instanceof GravestoneEditScreen) && DecorationsFurnitureModule.GRAVESTONE_BLOCK_ENTITY.is(screen.sign.getType())) {
---
+++ decorations/src/main/java/dev/apexstudios/fantasyfurniture/decorations/ber/SimpleBlockEntityModel.java
@@ -0,0 +1,51 @@
+package dev.apexstudios.fantasyfurniture.decorations.ber;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
+import net.minecraft.client.model.Model;
+import net.minecraft.client.model.geom.EntityModelSet;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.SubmitNodeCollector;
+import net.minecraft.client.renderer.feature.ModelFeatureRenderer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.core.Direction;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Unit;
+import org.jetbrains.annotations.Nullable;
+
+final class SimpleBlockEntityModel {
+    public final Model.Simple model;
+
+    SimpleBlockEntityModel(EntityModelSet models, ModelLayerLocation modelLocation) {
+        model = new Model.Simple(models.bakeLayer(modelLocation), RenderType::entityTranslucent);
+    }
+
+    public void submitModel(PoseStack poseStack, SubmitNodeCollector nodeCollector, ResourceLocation texture, @Nullable Direction facing, int packedLight, @Nullable ModelFeatureRenderer.CrumblingOverlay breakProgress) {
+        poseStack.pushPose();
+        preparePose(poseStack, facing);
+
+        nodeCollector.submitModel(
+                model,
+                Unit.INSTANCE,
+                poseStack,
+                model.renderType(texture),
+                packedLight,
+                OverlayTexture.NO_OVERLAY,
+                0,
+                breakProgress
+        );
+
+        poseStack.popPose();
+    }
+
+    public void preparePose(PoseStack poseStack, @Nullable Direction facing) {
+        poseStack.translate(0D, 1D, 0D);
+        poseStack.translate(.5D, .5D, .5D);
+        poseStack.mulPose(Axis.ZP.rotationDegrees(180F));
+
+        if(facing != null) {
+            poseStack.mulPose(Axis.YP.rotationDegrees(facing.toYRot()));
+        }
+    }
+}
