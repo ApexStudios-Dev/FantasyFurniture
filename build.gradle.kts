fb636cfb3824870e3fa0549b030f35a2de17a706
Apex <robsonlawson3@gmail.com>
2026-01-07 17:13:26 +0000

    Update to 26.1-snapshot-2

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 group = "dev.apexstudios"
 
-apex.neoVersion("26.1.0.0-alpha.1+snapshot-1")
+apex.neoVersion("26.1.0.0-alpha.5+snapshot-2")
 apex.extendCompilerErrors()
 
 val single = ApexSingleExtension.getOrCreate(project)
