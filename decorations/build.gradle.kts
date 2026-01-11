a3501e91145781a4a654ed9506d014f680a48eb0
Apex <robsonlawson3@gmail.com>
2026-01-11 19:59:29 +0000

    Include ApexCore accesstransformer dependencies

--- decorations/build.gradle.kts
+++ decorations/build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
     val apexcore = "26.1.10-beta-pr-70"
     implementation("dev.apexstudios:apexcore:$apexcore")
     "dataImplementation"("dev.apexstudios:apexcore:$apexcore")
-    // accessTransformers("dev.apexstudios:apexcore:$apexcore")
+    accessTransformers("dev.apexstudios:apexcore:$apexcore")
 
     val furnitureProject = findProject(":FantasyFurniture") ?: rootProject
     implementation(furnitureProject)
