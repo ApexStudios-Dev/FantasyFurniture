38a4e3267c829394ad87812362f01501e7850175
Apex <29412632+ApexModder@users.noreply.github.com>
2026-03-26 01:59:05 +0000

    Fix ConTex CTM (#109)
    Adds ContextMatters support

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,7 +21,8 @@ dependencyResolutionManagement {
         library("apexcore", "dev.apexstudios", "apexcore").version("26.1.0")
         bundle("apexcore", listOf("registree", "apexcore"))
 
-        library("contex", "curse.maven", "contex-1296805").version("7051640")
+        library("contex", "curse.maven", "contex-1296805").version("7806346")
+        library("contextmatters", "curse.maven", "context-matters-1265417").version("7808075")
     }
 }
 
