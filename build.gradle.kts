d63a3c86023aee19822348b87340e0af9c8c753a
Apex <robsonlawson3@gmail.com>
2026-04-11 15:12:42 +0100

    Fix build scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,15 +12,43 @@ group = "dev.apexstudios"
 neoForge {
     version = libs.versions.neoforge.get()
 
+    subprojects.forEach {
+        mods {
+            create("${it.name}_${SourceSet.MAIN_SOURCE_SET_NAME}") {
+                sourceSet(it.sourceSets[SourceSet.MAIN_SOURCE_SET_NAME])
+            }
+
+            create("${it.name}_data") {
+                sourceSet(it.sourceSets[SourceSet.MAIN_SOURCE_SET_NAME])
+                sourceSet(it.sourceSets["data"])
+            }
+        }
+    }
+
     afterEvaluate {
-        runs.getByName("data") {
-            // include bone built-in packs as they are needed for
-            // ctm asset generation to complete
-            programArguments.addAll(
-                "--mod", "fantasyfurniture",
-                "--existing", file("bone/src/data/generated/built-in/assets/skeleton").absolutePath,
-                "--existing", file("bone/src/data/generated/built-in/assets/wither").absolutePath
-            )
+        runs {
+            configureEach {
+                loadedMods.set(listOf(mods[SourceSet.MAIN_SOURCE_SET_NAME]))
+
+                subprojects.forEach {
+                    loadedMods.add(mods["${it.name}_${SourceSet.MAIN_SOURCE_SET_NAME}"])
+                }
+            }
+
+            getByName("data") {
+                loadedMods.set(listOf(mods["data"]))
+
+                subprojects.forEach {
+                    loadedMods.add(mods["${it.name}_data"])
+                }
+
+                // include bone built-in packs as they are needed for
+                // ctm asset generation to complete
+                programArguments.addAll(
+                    "--existing", file("bone/src/data/generated/built-in/assets/skeleton").absolutePath,
+                    "--existing", file("bone/src/data/generated/built-in/assets/wither").absolutePath
+                )
+            }
         }
     }
 }
@@ -29,13 +57,6 @@ dependencies {
     implementation(libs.bundles.apexcore)
     "dataImplementation"(libs.bundles.apexcore)
     accessTransformers(libs.apexcore)
-
-    afterEvaluate {
-        subprojects.forEach {
-            "runtimeOnly"(it)
-            "dataRuntimeOnly"(it)
-        }
-    }
 
     runtimeOnly(libs.contex)
     runtimeOnly(libs.contextmatters)
