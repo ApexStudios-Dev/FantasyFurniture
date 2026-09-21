42430478c386140fade4aa1ffa7f6c820be12bf6
Apex <robsonlawson3@gmail.com>
2026-09-21 20:09:18 +0100

    Bump NeoForge and update dyeable tag

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,7 +19,7 @@ pluginManagement {
 
 dependencyResolutionManagement {
     versionCatalogs.create("libs") {
-        version("neoforge", "26.3.0.7-beta")
+        version("neoforge", "26.3.0.8-beta")
 
         library("registree", "dev.apexstudios", "registree").version("26.3.0")
         library("apexcore", "dev.apexstudios", "apexcore").version("26.3.0")
